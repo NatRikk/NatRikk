6b11cfc1835affeee32bdf572cf92942de11d1bf
NatRikk <145622298+NatRikk@users.noreply.github.com>
2024-02-26 15:42:12 -0600

    added linear search algorithm

--- algorithmsDS.py
+++ algorithmsDS.py
@@ -1,3 +1,20 @@
-def main():
-    print("Hi")
-main()
+#Linear Search algorithm example ------------------------------------------------------------------------
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i  # Return the index if found
+    return -1  # Return -1 if not found
+
+# Example usage
+numbers = [3, 5, 2, 8, 6, 7, 4]
+target_number = 7
+
+result = linear_search(numbers, target_number)
+
+if result != -1:
+    print(f"Number found at index {result}")
+else:
+    print("Number not found in the list.")
+#--------------------------------------------------------------------------------------------------------
+
+#Binary search algorithm example ------------------------------------------------------------------------
