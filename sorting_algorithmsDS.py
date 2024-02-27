8cf86d973745caeea67a32c39ea7f2bf8aa04450
NatRikk <145622298+NatRikk@users.noreply.github.com>
2024-02-27 11:33:37 -0600

    added sorting algorithms file with a lot of stuff in it

--- sorting_algorithmsDS.py
+++ sorting_algorithmsDS.py
@@ -28,3 +28,72 @@ print('SORTED:', numbers)
 #------------------------------------------------------------------------------------------------------------
 
 # Insertion sort algorithm example --------------------------------------------------------------------------
+def insertion_sort(numbers):
+    for i in range(1, len(numbers)):
+        j = i
+
+        # Insert numbers[i] into sorted part 
+        # stopping once numbers[i] in correct position
+        while j > 0 and numbers[j] < numbers[j - 1]:
+            # Swap numbers[j] and numbers[j - 1]
+            temp = numbers[j]
+            numbers[j] = numbers[j - 1]
+            numbers[j - 1] = temp
+            j -= 1
+    
+# Create a list of unsorted values    
+numbers = [10, 2, 78, 4, 45, 32, 7, 11]
+
+# Print unsorted list
+print('UNSORTED:', numbers)
+
+# Call the insertion_sort function
+insertion_sort(numbers)
+
+# Print sorted list
+print('SORTED:', numbers)
+#------------------------------------------------------------------------------------------------------------
+
+# Shell sort algorithm example ------------------------------------------------------------------------------
+def insertion_sort_interleaved(numbers, start_index, gap):
+    for i in range(start_index + gap, len(numbers), gap):
+        j = i
+        while (j - gap >= start_index) and (numbers[j] < numbers[j - gap]):
+            temp = numbers[j]
+            numbers[j] = numbers[j - gap]
+            numbers[j - gap] = temp
+            j = j - gap
+
+def shell_sort(numbers, gap_values):
+    for gap_value in gap_values:
+        for i in range(gap_value):
+            insertion_sort_interleaved(numbers, i, gap_value)
+#-------------------------------------------------------------------------------------------------------------
+            
+# Quicksort algorithm example --------------------------------------------------------------------------------
+def quicksort(numbers, start_index, end_index):
+    # Only attempt to sort the list segment if there are
+    # at least 2 elements
+    if end_index <= start_index:
+        return
+          
+    # Partition the list segment
+    high = partition(numbers, start_index, end_index)
+
+    # Recursively sort the left segment
+    quicksort(numbers, start_index, high)
+
+    # Recursively sort the right segment
+    quicksort(numbers, high + 1, end_index)
+
+# Main program to test the quicksort algorithm.
+numbers = [12, 18, 3, 7, 32, 14, 91, 16, 8, 57]
+print('UNSORTED:', numbers)
+
+quicksort(numbers, 0, len(numbers)-1)
+print('SORTED:', numbers)
+#-------------------------------------------------------------------------------------------------------------
+
+# Merge sort algorithm example -------------------------------------------------------------------------------
+ 
+# Radix sort algorithm example -------------------------------------------------------------------------------
